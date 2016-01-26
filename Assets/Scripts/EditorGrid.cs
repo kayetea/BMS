25ba934e5be84fe95baddbef305d77944038d4b6
Katelyn <godfrey.katelyn@gmail.com>
2016-01-26 06:41:07 -0500

    Evan committing Katelyn's work from Mon 1/25

---
+++ Assets/Scripts/EditorGrid.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EditorGrid : MonoBehaviour {
+
+	private Text[] textRowArray;
+	
+
+	// Use this for initialization
+	void Start () {
+		foreach (Transform child in this.transform) {
+			//reset each text element
+			Debug.Log (child.GetComponentInChildren<Text>().text);
+			child.GetComponentInChildren<Text>().text = null;
+
+			//textRowArray[].add
+			//child.GetComponentInChildren<Text>().text;
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
